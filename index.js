94e0f678ccd899077b63226662edf1459c4e7874
Nincompup <lit2021032@iiitl.ac.in>
2023-01-27 13:48:07 +0530

    added * ahead

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin: *","Access-Control-Allow-Methods: GET, POST, PUT",
+  res.header("Access-Control-Allow-Origin","*","Access-Control-Allow-Methods: GET, POST, PUT",
   "Access-Control-Allow-Headers: Content-Type");
   next();
 });
