27b0baef84a79988ab6c5e644dcdf945fac1be69
Nincompup <lit2021032@iiitl.ac.in>
2023-01-27 13:39:26 +0530

    lappies bday

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin: https://banking-app-frontend-1dmng6pmv-nincompup.vercel.app","Access-Control-Allow-Methods: GET, POST, PUT",
+  res.header("Access-Control-Allow-Origin: *","Access-Control-Allow-Methods: GET, POST, PUT",
   "Access-Control-Allow-Headers: Content-Type");
   next();
 });
